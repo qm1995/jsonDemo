eab0af4ca98a304782592bd9f6d7014e5225642e
minqiu <1075812593@qq.com>
2018-12-29 15:10:17 +0800

    add Precautions

--- src/main/java/com/qm/jsondemo/demo/handler/RequestJson.java
+++ src/main/java/com/qm/jsondemo/demo/handler/RequestJson.java
@@ -6,6 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * 注意点，此注解不能和@RequestBody注解共用，即在一个方法内的参数
+ * 不能同时出现这两个注解，如
+ * error:
+ *     test1(@RequestJson Integer age,@RequestBody String name), 这是错误的，
  * @author qiumin
  * @create 2018/12/24 18:27
  * @desc
@@ -21,7 +25,7 @@ public @interface RequestJson {
     String fieldName() default "";
 
     /**
-     * 默认值，不填则默认为null。注，如果不填且为基础类型，则会抛出异常
+     * 默认值，不填则默认为null。
      * @return
      */
     String defaultValue() default "";
